a742fc18dbe281ef54be9a558a331ff846a20f8e
Yusuf Abraham Bismo Kristanto <yusuf.kristanto@shopee.com>
2023-01-26 13:23:09 +0700

    feat: add next links to layout and layout sidebar component on mobile

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,11 +1,32 @@
 import useScrollPosition from '@/hooks/ScrollPosition';
+import Head from 'next/head';
+import Link from 'next/link';
 import React from 'react';
+import { CgMenuGridO } from 'react-icons/cg';
+import LayoutSidebar from './LayoutSidebar';
 
 function Layout({ children }: { children: React.ReactNode }) {
   const scrollPosition = useScrollPosition();
 
+  const handleClickLayoutSidebar = () => {
+    const layoutSidebarEl = document.getElementById('layout-sidebar');
+    layoutSidebarEl?.classList.toggle('-left-full');
+    layoutSidebarEl?.classList.toggle('left-0');
+    const layoutOverlayEl = document.getElementById('layout-overlay');
+    layoutOverlayEl?.classList.toggle('hidden');
+  };
+
   return (
     <div className='font-epilogue'>
+      <Head>
+        <title>Abraham Kristanto Portfolio 2023</title>
+        <meta
+          name='description'
+          content='Abraham Kristanto Portfolio Home Page'
+        />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
       <nav
         className={`flex sticky top-0 p-5 w-full justify-between transition-all duration-100 ${
           scrollPosition > 0
@@ -13,13 +34,27 @@ function Layout({ children }: { children: React.ReactNode }) {
             : 'text-base-content'
         }`}
       >
-        <div className='text-lg'>Abraham Kristanto</div>
-        <div className='flex gap-4 text-lg'>
-          <div>About</div>
-          <div>Work</div>
-          <div>Contact</div>
+        <Link href='/'>
+          <div className='text-xl ml-2 hover:text-primary cursor-pointer'>
+            Abraham Kristanto
+          </div>
+        </Link>
+        <div className='hidden md:flex gap-8 text-lg'>
+          <Link href='/about'>
+            <div className='hover:text-primary cursor-pointer'>About</div>
+          </Link>
+          <Link href='/work'>
+            <div className='hover:text-primary cursor-pointer'>Work</div>
+          </Link>
+          <Link href='/contact'>
+            <div className='hover:text-primary cursor-pointer'>Contact</div>
+          </Link>
+        </div>
+        <div className='block md:hidden' onClick={handleClickLayoutSidebar}>
+          <CgMenuGridO size={25} />
         </div>
       </nav>
+      <LayoutSidebar handleClickLayoutSidebar={handleClickLayoutSidebar} />
       {children}
     </div>
   );
---
+++ components/LayoutSidebar.tsx
@@ -0,0 +1,28 @@
+type LayoutSidebarProps = {
+  handleClickLayoutSidebar: () => void;
+};
+
+function LayoutSidebar({ handleClickLayoutSidebar }: LayoutSidebarProps) {
+  return (
+    <>
+      <div
+        id='layout-sidebar'
+        className='fixed top-0 -left-full w-4/5 h-screen bg-base-200 shadow-md z-50 ease-in-out duration-300 md:relative md:w-auto md:h-auto md:top-0 md:left-0 md:rounded-lg md:hidden'
+      >
+        <div id='' className='p-5 flex flex-col gap-2'>
+          <div className='font-bold text-xl mb-2'>Menu</div>
+          <div>About</div>
+          <div>Work</div>
+          <div>Contact</div>
+        </div>
+      </div>
+      <div
+        id='layout-overlay'
+        className='top-0 z-40 w-screen h-screen bg-black fixed opacity-30 hidden'
+        onClick={handleClickLayoutSidebar}
+      ></div>
+    </>
+  );
+}
+
+export default LayoutSidebar;
